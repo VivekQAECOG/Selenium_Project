7b137ab3e78473ac3046c8d91b6f4fa2f5a6efbb
testuser <testuser@SFVM-COG-198.SFG.CORP.LOCAL>
2018-09-12 04:18:14 -0500

    First Upload

---
+++ com.cognizant.GitTest/src/main/java/com/cognizant/GitTest/com/cognizant/GitTest/App.java
@@ -0,0 +1,13 @@
+package com.cognizant.GitTest.com.cognizant.GitTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ com.cognizant.GitTest/src/test/java/com/cognizant/GitTest/com/cognizant/GitTest/Test.java
@@ -0,0 +1,10 @@
+package com.cognizant.GitTest.com.cognizant.GitTest;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Upload");
+	}
+
+}
