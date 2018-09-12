3a37e51b01f4078468b9636898e243675d681954
testuser <testuser@SFVM-COG-198.SFG.CORP.LOCAL>
2018-09-12 04:30:33 -0500

    Second Upload to Local GIT

--- com.cognizant.GitTest/src/test/java/com/cognizant/GitTest/com/cognizant/GitTest/Test.java
+++ com.cognizant.GitTest/src/test/java/com/cognizant/GitTest/com/cognizant/GitTest/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("First Upload");
+		System.out.println("Second Upload");
 	}
 
 }
